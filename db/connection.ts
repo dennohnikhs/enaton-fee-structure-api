d03a0bec05954820b6f7069eef5f53c8dc63f1ab
Rashon Kiptoo <biikiptoo596@gmail.com>
2024-02-05 21:05:47 +0300

    Bring back connection

--- db/connection.ts
+++ db/connection.ts
@@ -1,17 +1,15 @@
-// const { Sequelize } = require("sequelize");
-// const { config } = require("../config/config");
+const { Sequelize } = require("sequelize");
+const { config } = require("../config/config");
 
-// const sequelize = new Sequelize(
-//   config.development.database,
-//   config.development.username,
-//   config.development.password,
-//   {
-//     host: config.development.host,
-//     port: config.development.port,
-//     dialect: config.development.dialect,
-//   }
-// );
+const sequelize = new Sequelize(
+  config.development.database,
+  config.development.username,
+  config.development.password,
+  {
+    host: config.development.host,
+    port: config.development.port,
+    dialect: config.development.dialect,
+  }
+);
 
-// export default sequelize;
-
-export default {}
+export default sequelize;
